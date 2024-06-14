978e7c986d823c53ab2bd55730d1630e3347c138
Antoine Delannoy <antoine.delannoy@ubisoft.com>
2024-06-14 17:11:21 +0200

    refactor: make the add command take inline details
    Upgrade the add commande behavior so that it takes details as an inline argument.
    When provided with the "-" character as the details content, it prompts the user
    for the actual details.

--- src/hyperfocus/console/commands/add.py
+++ src/hyperfocus/console/commands/add.py
@@ -11,13 +11,13 @@ from hyperfocus.termui.components import SuccessNotification
 
 @click.command(help="Add task to current working day")
 @click.argument("title", metavar="<title>", type=click.STRING)
-@click.option("-d", "--details", "add_details", is_flag=True, help="add task details")
-def add(title: str, add_details: bool) -> None:
+@click.option("-d", "--details", "details", default="", type=click.STRING, help="add task details")
+def add(title: str, details: str) -> None:
     session = get_current_session()
 
-    details = prompt.prompt("Task details") if add_details else ""
+    details_content = prompt.prompt("Task details") if details == "-" else details
 
-    task = session.daily_tracker.create_task(title=title, details=details)
+    task = session.daily_tracker.create_task(title=title, details=details_content)
     printer.echo(
         SuccessNotification(
             text=f"{formatter.task(task=task, show_prefix=True)} created",
