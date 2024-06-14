8258e0d5285f4aa476d369004ad519baf45eda8c
Antoine Delannoy <antoine.delannoy@ubisoft.com>
2024-06-14 17:12:16 +0200

    tests: fix add command tests and add new tests

--- tests/test_console/test_commands/test_add.py
+++ tests/test_console/test_commands/test_add.py
@@ -21,9 +21,26 @@ def test_add(cli):
 
     result = runner.invoke(cli, ["add", "bar", "-d"], input="baz\n")
 
+    assert result.exit_code == 2
+
+
+@pytest.mark.functional
+def test_add_with_inline_details(cli):
+    result = runner.invoke(cli, ["add", "foo", "-d", "bar"])
+
     assert result.exit_code == 0
     assert result.output == (
-        f"{icons.PROMPT} Task details: baz\n"
+        f"{icons.SUCCESS}(success) " f"Task: #1 {icons.TASK_STATUS} foo created\n"
+    )
+
+
+@pytest.mark.functional
+def test_add_with_stdin_details(cli):
+    result = runner.invoke(cli, ["add", "foo", "-d", "-"], input="bar\n")
+
+    assert result.exit_code == 0
+    assert result.output == (
+        f"{icons.PROMPT} Task details: bar\n"
         f"{icons.SUCCESS}(success) "
-        f"Task: #2 {icons.TASK_STATUS} bar created\n"
+        f"Task: #1 {icons.TASK_STATUS} foo created\n"
     )
