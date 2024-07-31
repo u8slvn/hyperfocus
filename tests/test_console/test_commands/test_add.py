d014d34f8092c7a2524c14067590c265ca635a05
u8slvn <u8slvn@gmail.com>
2024-07-31 10:43:22 +0200

    feat: add --bulk option to add task command

--- tests/test_console/test_commands/test_add.py
+++ tests/test_console/test_commands/test_add.py
@@ -37,9 +37,42 @@ def test_add_with_inline_details(cli):
 @pytest.mark.functional
 def test_add_with_multiple_lines_details(mocker, cli):
     mocker.patch("click.edit", return_value="foo\nbar")
-    result = runner.invoke(cli, ["add", "foo", "-d", "-"], input="bar\n")
+    result = runner.invoke(cli, ["add", "foo", "-d", "-"])
 
     assert result.exit_code == 0
     assert result.output == (
         f"{icons.SUCCESS}(success) " f"Task: #1 {icons.TASK_STATUS} foo created\n"
     )
+
+
+@pytest.mark.functional
+def test_add_with_bulk_option(mocker, cli):
+    mocker.patch("click.edit", return_value="foo\nbar")
+    result = runner.invoke(cli, ["add", "-b"], input="bar\n")
+
+    assert result.exit_code == 0
+    assert result.output == (
+        f"{icons.SUCCESS}(success) Task: #1 {icons.TASK_STATUS} foo created\n"
+        f"{icons.SUCCESS}(success) Task: #2 {icons.TASK_STATUS} bar created\n"
+    )
+
+
+@pytest.mark.functional
+def test_add_with_bulk_option_ignore_empty_lines(mocker, cli):
+    mocker.patch("click.edit", return_value="   \n\n \ntask n1\n \n\t\ntask n2")
+    result = runner.invoke(cli, ["add", "-b"])
+
+    assert result.exit_code == 0
+    assert result.output == (
+        f"{icons.SUCCESS}(success) Task: #1 {icons.TASK_STATUS} task n1 created\n"
+        f"{icons.SUCCESS}(success) Task: #2 {icons.TASK_STATUS} task n2 created\n"
+    )
+
+
+@pytest.mark.functional
+def test_add_with_bulk_option_does_nothing_with_empty_lines(mocker, cli):
+    mocker.patch("click.edit", return_value="   \n\n \n   \n")
+    result = runner.invoke(cli, ["add", "-b"])
+
+    assert result.exit_code == 0
+    assert result.output == ""
