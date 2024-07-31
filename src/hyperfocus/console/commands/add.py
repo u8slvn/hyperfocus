a872781980d40b8fd539e3cddfd6bb941e161950
u8slvn <u8slvn@gmail.com>
2024-07-31 10:43:22 +0200

    feat: add --bulk option to add task command

--- src/hyperfocus/console/commands/add.py
+++ src/hyperfocus/console/commands/add.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import click
 
+from hyperfocus.console.core.parameters import NotRequired
+from hyperfocus.console.core.parameters import NotRequiredIf
 from hyperfocus.services.session import get_current_session
 from hyperfocus.termui import formatter
 from hyperfocus.termui import printer
@@ -9,18 +11,49 @@ from hyperfocus.termui.components import SuccessNotification
 
 
 @click.command(help="Add task to current working day")
-@click.argument("title", metavar="<title>", type=click.STRING)
-@click.option(
-    "-d", "--details", "details", default="", type=click.STRING, help="add task details"
+@click.argument(
+    "title",
+    cls=NotRequiredIf,
+    not_required_if=["bulk"],
+    metavar="<title>",
+    type=click.STRING,
 )
-def add(title: str, details: str) -> None:
+@click.option(
+    "-d",
+    "--details",
+    "details",
+    cls=NotRequired,
+    not_required=["bulk"],
+    default="",
+    type=click.STRING,
+    help="add task details",
+)
+@click.option(
+    "-b",
+    "--bulk",
+    "bulk",
+    cls=NotRequired,
+    not_required=["title", "details"],
+    is_flag=True,
+    help="add multiple tasks",
+)
+def add(title: str, details: str, bulk: bool) -> None:
     session = get_current_session()
 
-    details_content = click.edit() if details == "-" else details
+    tasks: list[tuple[str, str | None]] = []
 
-    task = session.daily_tracker.create_task(title=title, details=details_content)
-    printer.echo(
-        SuccessNotification(
-            text=f"{formatter.task(task=task, show_prefix=True)} created",
+    if bulk is True:
+        bulk_input = click.edit() or ""
+        task_titles = [line.strip() for line in bulk_input.splitlines() if line.strip()]
+        tasks.extend((title, "") for title in task_titles)
+    else:
+        details_content = click.edit() if details == "-" else details
+        tasks.append((title, details_content))
+
+    for title, details_content in tasks:
+        task = session.daily_tracker.create_task(title=title, details=details_content)
+        printer.echo(
+            SuccessNotification(
+                text=f"{formatter.task(task=task, show_prefix=True)} created",
+            )
         )
-    )
